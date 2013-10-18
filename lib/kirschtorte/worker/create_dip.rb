a464f14a60a436eedd8d30ad0a9983dc48fba392
Michael Slone <m.slone@gmail.com>
2013-10-18 13:03:09 -0400

    skip Create DIP step for dark-archive submissions

--- lib/kirschtorte/worker/create_dip.rb
+++ lib/kirschtorte/worker/create_dip.rb
@@ -9,17 +9,26 @@ module Kirschtorte
 
       def self.perform payload
         g = Kirschtorte::Worker::Generic.new payload
+        dip_id = g.package.get(:dip_identifier)
+        dark_archive = g.package.get(:dark_archive)
+
+        if dark_archive
+          puts "SKIPPED CreateDip for #{dip_id} (dark archive)"
+          g.task.complete!
+          return
+        end
+
         config = YAML.load_file File.join('config', 'abby_normal.yml')
         aip_tree = Pairtree.at(config['local']['aip_dir'], create: true)
         dip_tree = Pairtree.at(config['local']['dip_dir'], create: true)
 
         aip_path = aip_tree.get(g.package.get(:aip_identifier)).path
-        dip_path = dip_tree.mk(g.package.get(:dip_identifier)).path
+        dip_path = dip_tree.mk(dip_id).path
 
         dipmaker = KDL::DipMaker.new STDOUT,
                    aip_path,
                    File.dirname(dip_path),
-                   :dip_directory => g.package.get(:dip_identifier)
+                   :dip_directory => dip_id
         dipmaker.build
         g.task.complete!
       end
