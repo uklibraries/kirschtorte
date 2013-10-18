aa70c9e29e33ca4da98a22e2cd7a08929482ef77
Michael Slone <m.slone@gmail.com>
2013-10-18 12:58:06 -0400

    trim some unused code

--- lib/kirschtorte/worker/create_dip.rb
+++ lib/kirschtorte/worker/create_dip.rb
@@ -8,8 +8,6 @@ module Kirschtorte
       @queue = :ingest
 
       def self.perform payload
-        @output = STDOUT
-        @errors = []
         g = Kirschtorte::Worker::Generic.new payload
         config = YAML.load_file File.join('config', 'abby_normal.yml')
         aip_tree = Pairtree.at(config['local']['aip_dir'], create: true)
@@ -18,8 +16,8 @@ module Kirschtorte
         aip_path = aip_tree.get(g.package.get(:aip_identifier)).path
         dip_path = dip_tree.mk(g.package.get(:dip_identifier)).path
 
-        dipmaker = KDL::DipMaker.new STDOUT, 
-                   aip_path, 
+        dipmaker = KDL::DipMaker.new STDOUT,
+                   aip_path,
                    File.dirname(dip_path),
                    :dip_directory => g.package.get(:dip_identifier)
         dipmaker.build
