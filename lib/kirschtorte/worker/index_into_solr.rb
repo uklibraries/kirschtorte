f6a6523aae69aad88692d04944ae4876ef846053
Michael Slone <m.slone@gmail.com>
2014-10-03 15:37:38 -0400

    add secondary index point

--- lib/kirschtorte/worker/index_into_solr.rb
+++ lib/kirschtorte/worker/index_into_solr.rb
@@ -20,6 +20,37 @@ module Kirschtorte
         end
 
         config = YAML.load_file File.join('config', 'abby_normal.yml')
+
+        # secondary index
+        #
+        # We're doing this before production because it's
+        # the success or failure of the latter that
+        # determines whether or not the entire job needs
+        # to be retried.
+        #
+        username = config['secondary']['username']
+        server = config['secondary']['solr_host']
+        remote_path = File.join config['secondary']['solr_dir'],
+                                "pairtree_root",
+                                Pairtree::Path.id_to_path(dip_id)
+
+        remote_dir = config['secondary']['blacklight_dir']
+        commands = [
+          "cd #{remote_dir}",
+          "rake solr:index:json_dir DIR=#{remote_path}",
+        ].join('; ')
+
+        Net::SSH.start(server, username) do |ssh|
+          output = ssh.exec!(commands)
+          puts "IndexIntoSolr: #{output}"
+          if output.lines.last =~ /#{dip_id}/
+            puts "IndexIntoSolr (secondary): #{server}:#{remote_path} succeeded"
+          else
+            puts "IndexIntoSolr (secondary): #{server}:#{remote_path} failed"
+          end
+        end
+
+        # production index
         username = config['production']['username']
         server = config['production']['solr_host']
         remote_path = File.join config['production']['solr_dir'],
